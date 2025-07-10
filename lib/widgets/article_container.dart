55d1759002860bd158ae2dedca2b39b8a8428c8c
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2025-07-10 21:44:18 +0900

    実際のarticleを表示

--- lib/widgets/article_container.dart
+++ lib/widgets/article_container.dart
@@ -9,68 +9,72 @@ class ArticleContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 180,
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-      decoration: const BoxDecoration(
-        color: Color(0xFF55c500),
-        borderRadius: BorderRadius.all(Radius.circular(32)),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat("yyyy/MM/dd").format(article.createdAt),
-            style: TextStyle(color: Colors.white, fontSize: 12),
-          ),
-          Text(
-            article.title,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        decoration: const BoxDecoration(
+          color: Color(0xFF55c500),
+          borderRadius: BorderRadius.all(Radius.circular(32)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              DateFormat("yyyy/MM/dd").format(article.createdAt),
+              style: TextStyle(color: Colors.white, fontSize: 12),
             ),
-          ),
-          Text(
-            "#${article.tags.join(" #")}",
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 12,
-              fontStyle: FontStyle.italic,
+            Text(
+              article.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Column(
-                children: [
-                  const Icon(Icons.favorite, color: Colors.white),
-                  Text(
-                    article.likesCount.toString(),
-                    style: const TextStyle(color: Colors.white, fontSize: 12),
-                  ),
-                ],
+            Text(
+              "#${article.tags.join(" #")}",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 12,
+                fontStyle: FontStyle.italic,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  CircleAvatar(
-                    radius: 26,
-                    backgroundImage: NetworkImage(article.user.profileImageUrl),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    article.user.id,
-                    style: const TextStyle(fontSize: 12, color: Colors.white),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Column(
+                  children: [
+                    const Icon(Icons.favorite, color: Colors.white),
+                    Text(
+                      article.likesCount.toString(),
+                      style: const TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    CircleAvatar(
+                      radius: 26,
+                      backgroundImage: NetworkImage(
+                        article.user.profileImageUrl,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      article.user.id,
+                      style: const TextStyle(fontSize: 12, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/search_screen.dart
+++ lib/widgets/search_screen.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:test_qiita_app/models/article.dart';
-import 'package:test_qiita_app/models/user.dart';
 import 'package:test_qiita_app/widgets/article_container.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -35,17 +34,11 @@ class _SearchScreenState extends State<SearchScreen> {
               },
             ),
           ),
-          ArticleContainer(
-            article: Article(
-              title: 'テスト',
-              user: User(
-                id: 'qii-taro',
-                profileImageUrl:
-                    'https://firebasestorage.googleapis.com/v0/b/gs-expansion-test.appspot.com/o/unknown_person.png?alt=media',
-              ),
-              createdAt: DateTime.now(),
-              tags: ['Flutter', 'dart'],
-              url: 'https://example.com',
+          Expanded(
+            child: ListView(
+              children: articles
+                  .map((article) => ArticleContainer(article: article))
+                  .toList(),
             ),
           ),
         ],
