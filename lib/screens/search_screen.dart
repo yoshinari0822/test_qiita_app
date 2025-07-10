6e3d403e6c419be975dc196693a738fe2e8f04b5
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2025-07-10 23:06:34 +0900

    無限取得の実装

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -14,6 +14,72 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   List<Article> articles = [];
+  int currentPage = 1;
+  bool isLoading = false;
+  final ScrollController _scrollController = ScrollController();
+  String currentKeyword = "";
+  bool hasMoreData = true; // 追加: データがまだあるかどうかを示すフラグ
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    // スクロール位置がリストの終わりから600ピクセル以内になったらロードを開始
+    if (_scrollController.position.pixels >=
+        _scrollController.position.maxScrollExtent - 600) {
+      _loadMoreArticles();
+    }
+  }
+
+  Future<void> _loadMoreArticles() async {
+    if (isLoading || currentKeyword.isEmpty || !hasMoreData) {
+      return;
+    }
+    setState(() {
+      isLoading = true;
+    });
+    final results = await searchQiita(currentKeyword, currentPage);
+    setState(() {
+      articles.addAll(results);
+      currentPage++;
+      isLoading = false;
+      if (results.isEmpty) {
+        // コレクションが空であるかどうかを確認
+        hasMoreData = false;
+      }
+    });
+  }
+
+  Future<List<Article>> searchQiita(String keyword, int page) async {
+    final Uri uri = Uri.https("qiita.com", "/api/v2/items", {
+      "query": "title:$keyword",
+      "per_page": "10",
+      "page": page.toString(),
+    });
+
+    final String token = dotenv.env["QIITA_ACCESS_TOKEN"] ?? "";
+
+    final http.Response res = await http.get(
+      uri,
+      headers: {"Authorization": "Bearer $token"},
+    );
+
+    if (res.statusCode == 200) {
+      final List<dynamic> body = jsonDecode(res.body);
+      return body.map((dynamic json) => Article.fromJson(json)).toList();
+    } else {
+      return [];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,47 +93,30 @@ class _SearchScreenState extends State<SearchScreen> {
               style: TextStyle(fontSize: 18, color: Colors.black),
               decoration: InputDecoration(hintText: "キーワードを入力"),
               onSubmitted: (String value) async {
-                final results = await searchQiita(value);
+                setState(() {
+                  articles = [];
+                  currentPage = 1;
+                  currentKeyword = value;
+                });
+                final results = await searchQiita(currentKeyword, currentPage);
                 setState(() {
                   articles = results;
+                  currentPage++;
                 });
               },
             ),
           ),
           Expanded(
-            child: ListView(
-              children: articles
-                  .map((article) => ArticleContainer(article: article))
-                  .toList(),
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: articles.length,
+              itemBuilder: (context, index) {
+                return ArticleContainer(article: articles[index]);
+              },
             ),
           ),
         ],
       ),
     );
-  }
-
-  Future<List<Article>> searchQiita(String keyword) async {
-    //URL、クエリパラメータの設定
-    final Uri uri = Uri.https("qiita.com", "/api/v2/items", {
-      "query": "title:$keyword",
-      "per_page": "10",
-    });
-
-    //APIキーの取得
-    final String token = dotenv.env["QIITA_ACCESS_TOKEN"] ?? "";
-
-    //リクエストの作成
-    final http.Response res = await http.get(
-      uri,
-      headers: {"Authorization": "Bearer $token"},
-    );
-
-    //ステータスに応じた処理
-    if (res.statusCode == 200) {
-      final List<dynamic> body = jsonDecode(res.body);
-      return body.map((dynamic json) => Article.fromJson(json)).toList();
-    } else {
-      return [];
-    }
   }
 }
