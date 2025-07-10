c36b96c75fa56c90491aa2569e22249f7ff78287
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2025-07-10 22:28:02 +0900

    webViewの表示

---
+++ lib/screens/article_screen.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+import "package:test_qiita_app/models/article.dart";
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ArticleScreen extends StatefulWidget {
+  const ArticleScreen({super.key, required this.article});
+
+  final Article article;
+
+  @override
+  State<ArticleScreen> createState() => _ArticleScreenState();
+}
+
+class _ArticleScreenState extends State<ArticleScreen> {
+  late WebViewController controller = WebViewController()
+    ..loadRequest(Uri.parse(widget.article.url));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Article")),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
--- lib/widgets/article_container.dart
+++ lib/widgets/article_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import "package:intl/intl.dart";
 import "package:test_qiita_app/models/article.dart";
+import "package:test_qiita_app/screens/article_screen.dart";
 
 class ArticleContainer extends StatelessWidget {
   const ArticleContainer({super.key, required this.article});
@@ -11,69 +12,86 @@ class ArticleContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        decoration: const BoxDecoration(
-          color: Color(0xFF55c500),
-          borderRadius: BorderRadius.all(Radius.circular(32)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              DateFormat("yyyy/MM/dd").format(article.createdAt),
-              style: TextStyle(color: Colors.white, fontSize: 12),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ArticleScreen(article: article),
             ),
-            Text(
-              article.title,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+          );
+        },
+
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          decoration: const BoxDecoration(
+            color: Color(0xFF55c500),
+            borderRadius: BorderRadius.all(Radius.circular(32)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat("yyyy/MM/dd").format(article.createdAt),
+                style: TextStyle(color: Colors.white, fontSize: 12),
               ),
-            ),
-            Text(
-              "#${article.tags.join(" #")}",
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Column(
-                  children: [
-                    const Icon(Icons.favorite, color: Colors.white),
-                    Text(
-                      article.likesCount.toString(),
-                      style: const TextStyle(color: Colors.white, fontSize: 12),
-                    ),
-                  ],
+              Text(
+                article.title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    CircleAvatar(
-                      radius: 26,
-                      backgroundImage: NetworkImage(
-                        article.user.profileImageUrl,
+              ),
+              Text(
+                "#${article.tags.join(" #")}",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Column(
+                    children: [
+                      const Icon(Icons.favorite, color: Colors.white),
+                      Text(
+                        article.likesCount.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      article.user.id,
-                      style: const TextStyle(fontSize: 12, color: Colors.white),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CircleAvatar(
+                        radius: 26,
+                        backgroundImage: NetworkImage(
+                          article.user.profileImageUrl,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        article.user.id,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
